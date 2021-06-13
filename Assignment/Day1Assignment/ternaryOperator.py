8df019e4ec0bc44b396a8912d8f34c919ebf35ac
karthikaMani <84615146+karthikaManiJothi@users.noreply.github.com>
2021-06-13 18:20:28 +0530

    ternaryOperator.py

---
+++ Assignment/Day1Assignment/ternaryOperator.py
@@ -0,0 +1,4 @@
+n=int(input("Enter num1:"))
+m=int(input("Enter num2:"))
+result = 'num1 is greater than num2' if n>m else 'num1 is equal to num2' if n==m else 'num1 is less than num2'
+print(result)
