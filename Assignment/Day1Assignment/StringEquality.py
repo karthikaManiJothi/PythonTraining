bd1ed1d1198d2b394e49844fb7e0e0b6171a27f4
karthikaMani <84615146+karthikaManiJothi@users.noreply.github.com>
2021-06-13 18:24:08 +0530

    StringEquality.py program

---
+++ Assignment/Day1Assignment/StringEquality.py
@@ -0,0 +1,9 @@
+str1 =input("String 1:")
+str2 =input("String 2:")
+if str1==str2:
+    print("both strings are equal")
+else:
+    if str1>str2:
+        print("string-1 is greater than string-2")
+    else:
+        print("string-2 is greater than string-1")
