2b60f50b8ddfb78018ec674136d9be2381c5baa4
karthikaMani <84615146+karthikaManiJothi@users.noreply.github.com>
2021-06-13 18:40:02 +0530

    exec12.py program

---
+++ Assignment/Day2Assignment/exec12.py
@@ -0,0 +1,6 @@
+list1 = [2**i for i in range(1,10)]
+list2 = [i**2 for i in range(1,15)]
+list3 = [i*i for  i in range(1,40) if i%2==0]
+print("Powers of 2:",list1)
+print("Squares:",list2)
+print("Doubles:",list3)
