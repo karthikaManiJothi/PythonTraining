e63c502633a2028d915ba9aaf7a40877877f263c
karthikaMani <84615146+karthikaManiJothi@users.noreply.github.com>
2021-06-15 08:40:35 +0530

    exec4.py program

---
+++ Assignment/Day3Assignment/exec4.py
@@ -0,0 +1,12 @@
+#fibonaaci series
+
+def fibonacci(n):
+   if n <= 1:
+       return n
+   else:
+       return(fibonacci(n-1) + fibonacci(n-2))
+
+num=int(input("enter a number:"))
+print("Fibonacci sequence:")
+for i in range(num):
+       print(fibonacci(i))
